ab8293b93cc2e4f76f9f9e63566c657c69eafa65
Samuel <samuel.thomas1109@outlook.com>
2025-04-03 20:07:26 -0300

    Refactor facilities layout for improved responsiveness

--- src/pages/facilities.vue
+++ src/pages/facilities.vue
@@ -1,101 +1,111 @@
 <template>
-  <v-row class="align-center mb-4">
-    <v-col cols="6">
-      <v-text-field
-        v-model="search"
-        label="Buscar Facility"
-        variant="outlined"
-        density="compact"
-        clearable
-      />
-    </v-col>
-    <v-col
-      cols="6"
-      class="text-right"
-    >
-      <v-btn
-        color="primary"
-        @click="openDialog"
-      >
-        Adicionar Facility
-      </v-btn>
-    </v-col>
-  </v-row>
-
-  <v-data-table
-    :items="filteredFacilities"
-    :headers="headers"
-    items-per-page="5"
+  <v-container
+    fluid
+    class="page-container"
   >
-    <template #item.actions="{ item }">
-      <v-btn
-        icon
-        color="blue"
-        @click="editFacility(item)"
+    <v-row class="align-center mb-4 flex-grow-0">
+      <v-col cols="6">
+        <v-text-field
+          v-model="search"
+          label="Buscar Facility"
+          variant="outlined"
+          density="compact"
+          clearable
+        />
+      </v-col>
+      <v-col
+        cols="6"
+        class="text-right"
       >
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        color="red"
-        @click="deleteFacility(item)"
-      >
-        <v-icon>mdi-delete</v-icon>
-      </v-btn>
-    </template>
-  </v-data-table>
-
-  <!-- Modal de Adicionar/Editar -->
-  <v-dialog
-    v-model="dialog"
-    max-width="500px"
-  >
-    <v-card>
-      <v-card-title>{{ editMode ? 'Editar Facility' : 'Adicionar Facility' }}</v-card-title>
-      <v-card-text>
-        <v-text-field
-          v-model="facility.name"
-          label="Nome"
-          required
-        />
-        <v-text-field
-          v-model="facility.description"
-          label="Descrição"
-        />
-        <v-text-field
-          v-model="facility.type"
-          label="Tipo"
-          required
-        />
-        <v-text-field
-          v-model="facility.latitude"
-          label="Latitude"
-          type="number"
-          required
-        />
-        <v-text-field
-          v-model="facility.longitude"
-          label="Longitude"
-          type="number"
-          required
-        />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn
-          color="gray"
-          @click="dialog = false"
-        >
-          Cancelar
-        </v-btn>
         <v-btn
           color="primary"
-          @click="saveFacility"
+          @click="openDialog"
         >
-          Salvar
+          Adicionar Facility
         </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+      </v-col>
+    </v-row>
+
+    <v-row class="flex-grow-1 overflow-auto">
+      <v-data-table
+        :items="filteredFacilities"
+        :headers="headers"
+        items-per-page="5"
+        class="full-height"
+      >
+        <template #[`item.actions`]="{ item }">
+          <v-btn
+            icon
+            color="blue"
+            size="x-small"
+            class="mr-2"
+            @click="editFacility(item)"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            color="red"
+            size="x-small"
+            @click="deleteFacility(item)"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-row>
+
+    <v-dialog
+      v-model="dialog"
+      max-width="500px"
+    >
+      <v-card>
+        <v-card-title>{{ editMode ? 'Editar Facility' : 'Adicionar Facility' }}</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="facility.name"
+            label="Nome"
+            required
+          />
+          <v-text-field
+            v-model="facility.description"
+            label="Descrição"
+          />
+          <v-text-field
+            v-model="facility.type"
+            label="Tipo"
+            required
+          />
+          <v-text-field
+            v-model="facility.latitude"
+            label="Latitude"
+            type="number"
+            required
+          />
+          <v-text-field
+            v-model="facility.longitude"
+            label="Longitude"
+            type="number"
+            required
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="gray"
+            @click="dialog = false"
+          >
+            Cancelar
+          </v-btn>
+          <v-btn
+            color="primary"
+            @click="saveFacility"
+          >
+            Salvar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -182,7 +192,17 @@ export default {
 </script>
 
 <style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
 .text-right {
   text-align: right;
+}
+
+.full-height {
+  height: 100%;
 }
 </style>
