c3e6b1037ac795768c02b07b6274e74cd41eba0f
Samuel <samuel.thomas1109@outlook.com>
2025-04-03 19:51:16 -0300

    Refactor DefaultLayout structure and styling

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,23 +1,49 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" app>
+    <v-app-bar
+      color="primary"
+      app
+    >
       <v-app-bar-title>Minha Plataforma</v-app-bar-title>
     </v-app-bar>
 
     <v-navigation-drawer app>
       <v-list>
-        <v-list-item to="/facilities">Facilities</v-list-item>
-        <v-list-item to="/products">Products</v-list-item>
-        <v-list-item to="/clients">Clients</v-list-item>
+        <v-list-item to="/facilities">
+          Facilities
+        </v-list-item>
+        <v-list-item to="/products">
+          Products
+        </v-list-item>
+        <v-list-item to="/clients">
+          Clients
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-main>
-      <v-container fluid>
-        <slot/>
+
+    <v-main class="main-container">
+      <v-container
+        fluid
+        class="content-container"
+      >
+        <slot />
       </v-container>
     </v-main>
   </v-app>
 </template>
 <script setup lang="ts">
 </script>
+
+<style scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.content-container {
+  flex: 1;
+  overflow: auto;
+}
+</style>
