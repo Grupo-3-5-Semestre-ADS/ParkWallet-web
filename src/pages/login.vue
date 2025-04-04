d01f6171eaaeaad197bf408bd0596b9aa0260ee6
Samuel <samuel.thomas1109@outlook.com>
2025-04-03 21:20:17 -0300

    Refactor login.vue for improved readability

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-container class="fill-height"
-               fluid>
-    <v-row justify="center"
-           align="center">
-      <v-col cols="12"
-             sm="8"
-             md="4">
+  <v-container
+    class="fill-height"
+    fluid
+  >
+    <v-row
+      justify="center"
+      align="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+      >
         <v-card class="pa-6">
-          <v-card-title class="text-h5">Login</v-card-title>
+          <v-card-title class="text-h5">
+            Login
+          </v-card-title>
 
           <v-card-text>
             <v-form @submit.prevent="handleSubmit">
@@ -16,14 +24,14 @@
                 label="E-mail"
                 type="email"
                 required
-              ></v-text-field>
+              />
 
               <v-text-field
                 v-model="password"
                 label="Senha"
                 type="password"
                 required
-              ></v-text-field>
+              />
 
               <v-btn
                 type="submit"
