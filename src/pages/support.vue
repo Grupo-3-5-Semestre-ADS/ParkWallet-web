a28a2e425bf9964d4a614f4630765c43b885406b
Samuel <samuel.thomas1109@outlook.com>
2025-05-29 20:10:20 -0300

    Reformated code

--- src/pages/support.vue
+++ src/pages/support.vue
@@ -3,30 +3,52 @@
     fluid
     class="page-container rounded elevation-4"
   >
-    <v-row no-gutters class="fill-height">
+    <v-row
+      no-gutters
+      class="fill-height"
+    >
       <!-- Lista de Clientes -->
-      <v-col cols="12" md="4" class="pr-md-4 user-list-container" style="border-right: 1px solid #ccc;">
+      <v-col
+        cols="12"
+        md="4"
+        class="pr-md-4 user-list-container"
+        style="border-right: 1px solid #ccc;"
+      >
         <v-list>
-          <v-list-item-title class="pa-4 text-h6">Conversas</v-list-item-title>
-          <v-divider></v-divider>
-          <div v-if="isLoadingClients" class="text-center pa-4">
-            <v-progress-circular indeterminate color="primary"></v-progress-circular>
+          <v-list-item-title class="pa-4 text-h6">
+            Conversas
+          </v-list-item-title>
+          <v-divider />
+          <div
+            v-if="isLoadingClients"
+            class="text-center pa-4"
+          >
+            <v-progress-circular
+              indeterminate
+              color="primary"
+            />
             <p>Carregando conversas...</p>
           </div>
-          <div v-else-if="clients.length === 0 && !isLoadingClients" class="text-center text-grey pa-5">
+          <div
+            v-else-if="clients.length === 0 && !isLoadingClients"
+            class="text-center text-grey pa-5"
+          >
             Nenhuma conversa encontrada.
           </div>
           <v-list-item
-            v-else
             v-for="client in clients"
+            v-else
             :key="client.id"
-            @click="selectClient(client)"
             :class="{ 'bg-grey-lighten-3': selectedClient?.id === client.id }"
             class="rounded my-1"
             lines="two"
+            @click="selectClient(client)"
           >
-            <template v-slot:prepend>
-              <v-avatar color="primary" class="mr-3">
+            <template #prepend>
+              <v-avatar
+                color="primary"
+                class="mr-3"
+              >
                 <span class="text-h6">{{ client.name.substring(0, 1).toUpperCase() }}</span>
               </v-avatar>
             </template>
@@ -42,23 +64,49 @@
       </v-col>
 
       <!-- Área do Chat -->
-      <v-col cols="12" md="8" class="d-flex flex-column pl-md-4 chat-area-container">
-        <div v-if="selectedClient" class="d-flex flex-column fill-height">
-          <v-toolbar density="compact" color="transparent">
-            <v-avatar color="primary" class="mr-3 ml-2">
+      <v-col
+        cols="12"
+        md="8"
+        class="d-flex flex-column pl-md-4 chat-area-container"
+      >
+        <div
+          v-if="selectedClient"
+          class="d-flex flex-column fill-height"
+        >
+          <v-toolbar
+            density="compact"
+            color="transparent"
+          >
+            <v-avatar
+              color="primary"
+              class="mr-3 ml-2"
+            >
               <span class="text-h6">{{ selectedClient.name.substring(0, 1).toUpperCase() }}</span>
             </v-avatar>
             <v-toolbar-title>{{ selectedClient.name }}</v-toolbar-title>
           </v-toolbar>
-          <v-divider></v-divider>
+          <v-divider />
 
-          <div ref="messageContainer" class="flex-grow-1 overflow-y-auto my-4 px-2 message-list"
-               style="border: 1px solid #e0e0e0; border-radius: 8px;">
-            <div v-if="isLoadingMessages" class="text-center pa-4">
-              <v-progress-circular indeterminate color="primary"></v-progress-circular>
+          <div
+            ref="messageContainer"
+            class="flex-grow-1 overflow-y-auto my-4 px-2 message-list"
+            style="border: 1px solid #e0e0e0; border-radius: 8px;"
+          >
+            <div
+              v-if="isLoadingMessages"
+              class="text-center pa-4"
+            >
+              <v-progress-circular
+                indeterminate
+                color="primary"
+              />
               <p>Carregando mensagens...</p>
             </div>
-            <div v-for="(msg, index) in messages" :key="msg.id || index" class="my-2">
+            <div
+              v-for="(msg, index) in messages"
+              :key="msg.id || index"
+              class="my-2"
+            >
               <div :class="msg.from === 'me' ? 'd-flex justify-end' : 'd-flex justify-start'">
                 <v-chip
                   :color="msg.from === 'me' ? 'primary' : 'grey-lighten-1'"
@@ -66,16 +114,22 @@
                   class="ma-1 pa-3"
                   style="max-width: 70%; height: auto; white-space: normal;"
                 >
-                  <div class="message-text">{{ msg.text }}</div>
-                  <div class="text-caption mt-1"
-                       :class="msg.from === 'me' ? 'text-blue-lighten-4' : 'text-grey-darken-1'">
+                  <div class="message-text">
+                    {{ msg.text }}
+                  </div>
+                  <div
+                    class="text-caption mt-1"
+                    :class="msg.from === 'me' ? 'text-blue-lighten-4' : 'text-grey-darken-1'"
+                  >
                     {{ formatTimestamp(msg.timestamp) }}
                   </div>
                 </v-chip>
               </div>
             </div>
-            <div v-if="!isLoadingMessages && messages.length === 0 && selectedClient"
-                 class="text-center text-grey pa-5">
+            <div
+              v-if="!isLoadingMessages && messages.length === 0 && selectedClient"
+              class="text-center text-grey pa-5"
+            >
               Nenhuma mensagem ainda. Comece a conversa!
             </div>
           </div>
@@ -90,13 +144,26 @@
               class="flex-grow-1 mr-2"
               @keyup.enter="sendMessage"
             />
-            <v-btn icon="mdi-send" color="primary" @click="sendMessage" :disabled="!newMessage.trim()"></v-btn>
+            <v-btn
+              icon="mdi-send"
+              color="primary"
+              :disabled="!newMessage.trim()"
+              @click="sendMessage"
+            />
           </div>
         </div>
 
-        <div v-else class="d-flex fill-height align-center justify-center text-center text-grey">
+        <div
+          v-else
+          class="d-flex fill-height align-center justify-center text-center text-grey"
+        >
           <div>
-            <v-icon size="x-large" class="mb-2">mdi-forum-outline</v-icon>
+            <v-icon
+              size="x-large"
+              class="mb-2"
+            >
+              mdi-forum-outline
+            </v-icon>
             <h4>Selecione uma conversa para começar</h4>
           </div>
         </div>
@@ -251,13 +318,13 @@ function sendMessage() {
     recipientUserId: selectedClient.value.id,
     timestamp: new Date().toISOString(),
   };
-  
+
   messages.value.push(localMessage);
   scrollToBottom();
-  
+
   // Limpar campo antes de enviar
   newMessage.value = '';
-  
+
   // Enviar mensagem via socket
   console.log('Enviando mensagem:', messagePayload);
   socket.emit('send_message', messagePayload);
@@ -298,16 +365,16 @@ function setupSocketListeners() {
 
   socket.on('message_sent_ack', (serverMsg: ServerMessage) => {
     console.log('Mensagem enviada confirmada:', serverMsg);
-    
+
     // Atualizar a mensagem local temporária com o ID real do servidor
     if (selectedClient.value && serverMsg.recipientUserId === selectedClient.value.id) {
-      const tempMsgIndex = messages.value.findIndex(m => 
-        m.from === 'me' && 
-        m.text === serverMsg.message && 
-        typeof m.id === 'string' && 
+      const tempMsgIndex = messages.value.findIndex(m =>
+        m.from === 'me' &&
+        m.text === serverMsg.message &&
+        typeof m.id === 'string' &&
         m.id.startsWith('temp-')
       );
-      
+
       if (tempMsgIndex !== -1) {
         // Atualizar com dados reais do servidor
         messages.value[tempMsgIndex] = {
